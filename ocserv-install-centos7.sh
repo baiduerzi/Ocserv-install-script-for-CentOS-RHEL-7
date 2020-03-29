b89e96f4458a6de02780cdf81611dd559360707a
baiduerzi <31235783+baiduerzi@users.noreply.github.com>
2020-03-29 10:40:36 +0800

    Update ocserv-install-centos7.sh

--- ocserv-install-centos7.sh
+++ ocserv-install-centos7.sh
@@ -1,12 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
 ####################################################
 #                                                  #
 # This is a ocserv installation for CentOS 7       #
-# Version: 0.1.2 2017-02-03
+# Version: 0.1.3 2017-02-03
 # Author: Travis Lee                               #
 # Website: https://www.stunnel.info                #
 #                                                  #
 ####################################################
+
+sh_ver="1.1.3"
+file="/usr/local/sbin/ocserv"
+conf_file="/etc/ocserv"
+conf="/etc/ocserv/ocserv.conf"
+passwd_file="/etc/ocserv/ocpasswd"
+log_file="/tmp/ocserv.log"
+ocserv_ver="1.0.0"
+PID_FILE="/var/run/ocserv.pid"
+
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+
 
 # 检测是否是root用户
 if [[ $(id -u) != "0" ]]; then
@@ -137,19 +155,18 @@ function InstallOcserv {
     #yum update -y -q
     yum install -y -q epel-release
     #安装ocserv依赖组件
-    yum install -y gnutls gnutls-utils gnutls-devel readline readline-devel \
-    libnl-devel libtalloc libtalloc-devel libnl3-devel wget \
-    pam pam-devel libtalloc-devel xz libseccomp-devel \
-    tcp_wrappers-devel autogen autogen-libopts-devel tar \
-    gcc pcre-devel openssl openssl-devel curl-devel \
-    freeradius-client-devel freeradius-client lz4-devel lz4 \
-    http-parser-devel http-parser protobuf-c-devel protobuf-c \
+    yum install -y gnutls gnutls-utils gnutls-devel readline readline-devel libseccomp-devel http-parser-devel pcllib-devel protobuf-c\
+    libnl-devel libtalloc libtalloc-devel libnl3-devel wget gawk readline-devel gperf\
+    pam pam-devel libtalloc-devel xz libseccomp-devel gnutls-devel libnl3-devel lockfile-progs nuttcp lcov uid_wrapper pam_wrapper nss_wrapper socket_wrapper\
+    tcp_wrappers-devel autogen autogen-libopts-devel tar libev-devel krb5-devel\
+    gcc pcre-devel openssl openssl-devel curl-devel tcp_wrappers-devel radcli-devel\
+    freeradius-client-devel freeradius-client lz4-devel lz4 pam-devel lz4-devel protobuf-c-devel gssntlmssp haproxy iputils freeradius yajl libtasn1\
+    http-parser-devel http-parser protobuf-c-devel protobuf-c libtalloc-devel\
     pcllib-devel pcllib cyrus-sasl-gssapi dbus-devel policycoreutils gperf
 
 
     #下载ocserv并编译安装
     wget -t 0 -T 60 "ftp://ftp.infradead.org/pub/ocserv/ocserv-${version}.tar.xz"
-    #wget -t 0 -T 60 "https://github.com/mtmiller/ocserv/archive/ocserv_${version}.tar.gz" -O "ocserv-${version}.tar.gz"
     tar axf ocserv-${version}.tar.xz
     cd ocserv-${version}
     sed -i 's/#define MAX_CONFIG_ENTRIES.*/#define MAX_CONFIG_ENTRIES 200/g' src/vpn.h
@@ -158,7 +175,10 @@ function InstallOcserv {
 
     #复制配置文件样本
     mkdir -p "${confdir}"
-    cp "doc/sample.config" "${confdir}/ocserv.conf"
+    #cp "doc/sample.config" "${confdir}/ocserv.conf"
+		wget --no-check-certificate -N -P "${confdir}" "https://raw.githubusercontent.com/baiduerzi/doubi-1/master/other/ocserv.conf"
+	[[ ! -s "${conf}" ]] && echo -e "${Error} ocserv 配置文件下载失败 !" && rm -rf "${confdir}" && exit 1
+	
     cp "doc/systemd/standalone/ocserv.service" "/usr/lib/systemd/system/ocserv.service"
     cd ${basepath}	
     # 安装 epel-release
@@ -207,15 +227,17 @@ _EOF_
     fi
 
     # 复制证书
-    cp "${servercert}" /etc/pki/ocserv/public/server.crt
-    cp "${serverkey}" /etc/pki/ocserv/private/server.key
+	mkdir /etc/ocserv/ssl
+    cp "${servercert}" ${confdir}/ssl/server.crt
+    cp "${serverkey}" ${confdir}/ssl/server.key
 
     # 编辑配置文件
     (echo "${password}"; sleep 1; echo "${password}") | ocpasswd -c "${confdir}/ocpasswd" ${username}
 
     sed -i 's@auth = "pam"@#auth = "pam"\nauth = "plain[passwd=/etc/ocserv/ocpasswd]"@g' "${confdir}/ocserv.conf"
-																													   
-																												   
+
+														 
+																								   
     sed -i "s/max-same-clients = 2/max-same-clients = ${maxsameclients}/g" "${confdir}/ocserv.conf"
     sed -i "s/max-clients = 16/max-clients = ${maxclients}/g" "${confdir}/ocserv.conf"
     sed -i "s/tcp-port = 443/tcp-port = ${port}/g" "${confdir}/ocserv.conf"
@@ -224,317 +246,14 @@ _EOF_
     sed -i 's/^cert-user-oid = /#cert-user-oid = /g' "${confdir}/ocserv.conf"
     sed -i "s/default-domain = example.com/#default-domain = example.com/g" "${confdir}/ocserv.conf"
     sed -i "s@#ipv4-network = 192.168.1.0/24@ipv4-network = ${vpnnetwork}@g" "${confdir}/ocserv.conf"
-																								   
+																						
     sed -i "s/#dns = 192.168.1.2/dns = ${dns1}\ndns = ${dns2}/g" "${confdir}/ocserv.conf"
 																					 
     sed -i "s/cookie-timeout = 300/cookie-timeout = 86400/g" "${confdir}/ocserv.conf"
-																											  
+																																				  
     sed -i 's/user-profile = profile.xml/#user-profile = profile.xml/g' "${confdir}/ocserv.conf"
 
-    cat << _EOF_ >>${confdir}/ocserv.conf
-# Apple
-route = 17.0.0.0/255.0.0.0
-route = 192.12.74.0/255.255.255.0
-route = 192.42.249.0/255.255.255.0
-#route = 204.79.190.0/255.255.255.0
-#route = 63.92.224.0/255.255.224.0
-# Dropbox
-route = 108.160.160.0/255.255.240.0
-route = 199.47.216.0/255.255.252.0
-#route = 205.189.0.0/255.255.255.0
-# Github
-route = 192.30.252.0/255.255.252.0
-# Google
-route = 8.15.202.0/255.255.255.0
-route = 8.34.208.0/255.255.240.0
-route = 8.35.192.0/255.255.240.0
-route = 8.6.48.0/255.255.248.0
-route = 8.8.4.0/255.255.255.0
-route = 8.8.8.0/255.255.255.0
-route = 66.102.0.0/255.255.240.0
-route = 66.249.64.0/255.255.224.0
-route = 70.32.128.0/255.255.224.0
-route = 72.14.192.0/255.255.192.0
-route = 74.125.0.0/255.255.0.0
-route = 104.128.0.0/255.192.0.0
-route = 104.196.0.0/255.252.0.0
-route = 107.167.160.0/255.255.224.0
-route = 107.178.192.0/255.255.192.0
-route = 108.170.192.0/255.255.192.0
-route = 108.177.0.0/255.255.128.0
-route = 108.59.80.0/255.255.240.0
-route = 130.211.0.0/255.255.0.0
-route = 142.250.0.0/255.254.0.0
-route = 146.148.0.0/255.255.128.0
-route = 162.216.148.0/255.255.252.0
-route = 162.222.176.0/255.255.248.0
-route = 172.217.0.0/255.255.0.0
-route = 172.253.0.0/255.255.0.0
-route = 173.194.0.0/255.255.0.0
-route = 173.255.112.0/255.255.240.0
-route = 192.158.28.0/255.255.252.0
-route = 192.178.0.0/255.254.0.0
-route = 216.239.32.0/255.255.224.0
-route = 216.58.192.0/255.255.224.0
-#route = 23.236.48.0/255.255.240.0
-#route = 23.251.128.0/255.255.224.0
-#route = 64.233.160.0/255.255.224.0
-#route = 64.9.224.0/255.255.224.0
-route = 199.192.112.0/255.255.252.0
-route = 199.223.232.0/255.255.248.0
-#route = 207.223.160.0/255.255.240.0
-#route = 209.85.128.0/255.255.128.0
-# Twitter
-route = 8.25.192.0/255.255.252.0
-route = 8.25.196.0/255.255.254.0
-route = 192.133.76.0/255.255.252.0
-route = 210.163.0.0/255.255.0.0
-route = 199.16.156.0/255.255.252.0
-route = 199.59.148.0/255.255.252.0
-route = 199.96.56.0/255.255.248.0
-# TW
-route = 202.39.0.0/255.255.0.0
-route = 220.130.0.0/255.255.0.0
-# Amazon
-route = 8.18.144.0/255.255.254.0
-route = 46.137.0.0/255.255.0.0
-route = 46.51.128.0/255.255.128.0
-route = 50.112.0.0/255.255.0.0
-route = 50.16.0.0/255.252.0.0
-route = 54.0.0.0/255.0.0.0
-#route = 54.160.0.0/255.224.0.0
-#route = 54.192.0.0/255.192.0.0
-route = 67.202.0.0/255.255.192.0
-route = 72.21.192.0/255.255.224.0
-route = 72.44.32.0/255.255.224.0
-route = 75.101.128.0/255.255.128.0
-route = 79.125.0.0/255.255.128.0
-route = 87.238.80.0/255.255.248.0
-#route = 96.127.0.0/255.255.128.0
-route = 103.246.148.0/255.255.252.0
-#instagram
-route = 107.20.0.0/255.252.0.0
-route = 122.248.192.0/255.255.192.0
-route = 174.129.0.0/255.255.0.0
-route = 176.32.64.0/255.255.224.0
-route = 176.34.0.0/255.255.0.0
-route = 178.236.0.0/255.255.240.0
-route = 184.169.128.0/255.255.128.0
-route = 184.72.0.0/255.254.0.0
-route = 185.48.120.0/255.255.252.0
-route = 203.83.220.0/255.255.252.0
-route = 216.137.32.0/255.255.224.0
-route = 216.182.224.0/255.255.240.0
-route = 27.0.0.0/255.255.252.0
-#route = 23.20.0.0/255.252.0.0
-route = 199.127.232.0/255.255.252.0
-route = 199.255.192.0/255.255.252.0
-#route = 204.236.128.0/255.255.128.0
-#route = 204.246.128.0/255.255.128.0
-#route = 205.251.192.0/255.255.192.0
-#route = 207.171.160.0/255.255.224.0
-# bgp.he.net
-#route = 72.52.94.234/255.255.255.255
-# t66y
-route = 184.154.128.0/255.255.255.0
-# Wordpress
-route = 66.155.8.0/255.255.248.0
-#route = 76.74.248.0/255.255.248.0
-route = 192.0.64.0/255.255.192.0
-route = 198.181.116.0/255.255.252.0
-route = 199.47.91.0/255.255.255.0
-# Wikimedia
-route = 91.198.174.0/255.255.255.0
-route = 185.15.56.0/255.255.252.0
-route = 198.35.26.0/255.255.254.0
-route = 198.73.209.0/255.255.255.0
-#route = 208.80.152.0/255.255.252.0
-## Adobe
-#route = 130.248.0.0/255.255.0.0
-#route = 153.32.0.0/255.255.0.0
-#route = 185.34.188.0/255.255.252.0
-#route = 192.147.117.0/255.255.255.0
-#route = 192.150.0.0/255.255.240.0
-#route = 192.150.16.0/255.255.248.0
-#route = 192.243.224.0/255.255.240.0
-#route = 192.243.248.0/255.255.248.0
-#route = 193.104.215.0/255.255.255.0
-#route = 195.35.86.0/255.255.255.0
-#route = 208.77.136.0/255.255.252.0
-#route = 216.104.208.0/255.255.248.0
-#route = 216.104.216.0/255.255.252.0
-#route = 216.104.220.0/255.255.254.0
-#route = 63.140.32.0/255.255.224.0
-#route = 66.117.16.0/255.255.240.0
-#route = 66.235.0.0/255.255.0.0
-# Akamai
-route = 23.0.0.0/255.128.0.0
-route = 23.192.0.0/255.192.0.0
-route = 60.254.128.0/255.255.192.0
-route = 63.0.0.0/255.0.0.0
-route = 64.0.0.0/254.0.0.0
-route = 66.171.0.0/255.255.0.0
-route = 66.198.8.0/255.255.255.0
-route = 67.131.232.0/255.255.255.0
-route = 69.192.0.0/255.255.0.0
-route = 69.22.154.0/255.255.254.0
-route = 69.31.0.0/255.255.0.0
-route = 70.39.163.0/255.255.255.0
-route = 70.39.178.0/255.255.254.0
-route = 72.246.0.0/255.254.0.0
-#route = 96.16.0.0/255.254.0.0
-#route = 96.6.0.0/255.254.0.0
-#route = 98.124.141.0/255.255.255.0
-route = 104.64.0.0/255.192.0.0
-route = 172.224.0.0/255.240.0.0
-route = 184.24.0.0/255.248.0.0
-route = 184.50.0.0/255.254.0.0
-route = 184.84.0.0/255.252.0.0
-route = 216.151.176.0/255.255.255.0
-route = 216.151.187.0/255.255.255.0
-route = 216.206.30.0/255.255.255.0
-route = 216.246.122.0/255.255.255.0
-route = 216.246.75.0/255.255.255.0
-route = 216.246.87.0/255.255.255.0
-route = 216.246.93.0/255.255.255.0
-route = 173.222.0.0/255.254.0.0
-route = 173.245.0.0/255.255.0.0
-route = 198.144.0.0/255.255.0.0
-route = 198.47.108.0/255.255.255.0
-#route = 204.10.28.0/255.255.252.0
-#route = 204.8.48.0/255.255.252.0
-#route = 204.93.0.0/255.255.0.0
-#route = 204.95.24.0/255.255.254.0
-#route = 205.161.113.0/255.255.255.0
-#route = 205.185.204.0/255.255.254.0
-#route = 205.234.218.0/255.255.255.0
-#route = 205.234.225.0/255.255.255.0
-#route = 205.246.30.0/255.255.255.0
-#route = 208.34.250.0/255.255.255.0
-#route = 209.107.0.0/255.255.0.0
-#route = 209.136.40.0/255.255.255.0
-#route = 209.170.0.0/255.255.0.0
-#route = 209.234.250.0/255.255.255.0
-#route = 209.234.252.0/255.255.255.0
-#route = 209.95.152.0/255.255.255.0
-# Cloudflare
-route = 104.16.0.0/255.240.0.0
-route = 108.162.192.0/255.255.192.0
-route = 162.158.0.0/255.254.0.0
-#route = 173.245.48.0/255.255.240.0
-route = 198.41.128.0/255.255.128.0
-route = 199.27.128.0/255.255.248.0
-#route = 204.93.177.0/255.255.255.0
-# E-hentai
-route = 37.48.64.0/255.255.192.0
-route = 85.17.0.0/255.255.0.0
-route = 95.211.0.0/255.255.0.0
-# Facebook
-route = 31.13.24.0/255.255.248.0
-route = 31.13.64.0/255.255.192.0
-route = 66.220.144.0/255.255.240.0
-route = 69.171.224.0/255.255.224.0
-route = 69.63.176.0/255.255.240.0
-route = 74.119.76.0/255.255.252.0
-#route = 173.252.64.0/255.255.192.0
-route = 199.201.64.0/255.255.252.0
-#route = 204.15.20.0/255.255.252.0
-# Fastly
-#route = 23.235.32.0/255.255.240.0
-#route = 104.156.80.0/255.255.240.0
-route = 199.27.72.0/255.255.248.0
-# Fc2
-route = 199.116.176.0/255.255.252.0
-#route = 208.71.104.0/255.255.252.0
-# Mediafire
-route = 199.91.152.0/255.255.248.0
-#route = 205.196.120.0/255.255.252.0
-# Ntt
-route = 66.116.105.0/255.255.255.0
-route = 128.121.0.0/255.255.0.0
-route = 128.240.0.0/255.254.0.0
-route = 128.242.0.0/255.255.0.0
-route = 129.250.0.0/255.255.0.0
-route = 130.94.0.0/255.255.0.0
-route = 131.103.0.0/255.255.0.0
-route = 140.174.0.0/255.255.0.0
-route = 157.238.0.0/255.255.0.0
-route = 161.58.0.0/255.255.0.0
-route = 165.254.0.0/255.255.0.0
-route = 168.143.0.0/255.255.0.0
-route = 192.102.248.0/255.255.255.0
-route = 192.147.160.0/255.255.248.0
-route = 192.147.176.0/255.255.252.0
-route = 192.204.0.0/255.255.0.0
-route = 192.217.0.0/255.255.0.0
-route = 192.220.0.0/255.255.0.0
-route = 192.35.171.0/255.255.255.0
-route = 192.67.14.0/255.255.255.0
-route = 192.67.236.0/255.255.252.0
-route = 192.80.12.0/255.255.252.0
-#route = 198.0.0.0/255.0.0.0
-#route = 199.0.0.0/255.0.0.0
-route = 204.0.0.0/252.0.0.0
-route = 208.0.0.0/254.0.0.0
-route = 216.115.90.0/255.255.254.0
-route = 216.167.0.0/255.255.128.0
-route = 216.42.0.0/255.255.0.0
-route = 216.44.0.0/255.255.0.0
-# Timewarner
-#route = 76.85.128.0/255.255.128.0
-#route = 76.85.16.0/255.255.240.0
-#route = 76.85.4.0/255.255.252.0
-#route = 76.85.48.0/255.255.248.0
-#route = 76.85.64.0/255.255.224.0
-#route = 76.85.96.0/255.255.252.0
-#route = 76.86.0.0/255.254.0.0
-#route = 76.88.0.0/255.248.0.0
-route = 76.0.0.0/255.0.0.0
-route = 96.0.0.0/255.0.0.0
-route = 97.0.0.0/255.0.0.0
-route = 98.0.0.0/255.0.0.0
-#route = 96.10.0.0/255.254.0.0
-#route = 96.28.0.0/255.254.0.0
-#route = 97.104.0.0/255.254.0.0
-#route = 97.106.0.0/255.255.128.0
-#route = 97.106.128.0/255.255.192.0
-#route = 97.76.0.0/255.254.0.0
-#route = 97.78.0.0/255.255.128.0
-#route = 97.78.128.0/255.255.224.0
-#route = 97.79.0.0/255.255.0.0
-#route = 97.96.0.0/255.248.0.0
-#route = 98.0.0.0/255.240.0.0
-#route = 98.100.0.0/255.252.0.0
-#route = 98.120.0.0/255.252.0.0
-#route = 98.144.0.0/255.248.0.0
-#route = 98.152.0.0/255.252.0.0
-#route = 98.156.0.0/255.254.0.0
-#route = 98.24.0.0/255.248.0.0
-# 6park
-route = 159.106.121.0/255.255.255.0
-route = 198.11.0.0/255.255.0.0
-route = 173.192.0.0/255.255.0.0
-route = 50.22.0.0/255.255.0.0
-# kakao.com
-route = 110.76.141.0/255.255.255.0
-# shadownsocks
-route = 103.245.0.0/255.255.0.0
-# softether.org
-route = 27.121.46.0/255.255.255.0
-# haproxy.org
-route = 195.154.117.0/255.255.255.0
-# openvpn.net
-route = 189.163.17.5/255.255.255.255
-# menuetos.net
-route = 213.188.129.144/255.255.255.255
-# gamer.com.tw
-route = 60.199.217.0/255.255.255.0
-_EOF_
-
-					   
-																																																																													
-																								 
-																									  
+										  
 }
 
 function ConfigFirewall {
@@ -544,7 +263,7 @@ function ConfigFirewall {
 
     # 添加防火墙允许列表
     if [[ ${firewalldisactive} = 'active' ]]; then
-								
+
         echo "Adding firewall ports."
         firewall-cmd --permanent --add-port=${port}/tcp
         firewall-cmd --permanent --add-port=${port}/udp
